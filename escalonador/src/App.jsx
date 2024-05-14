534a54096846b7030b6492dfa20c6aa942b5014c
Artur <arturrodriguesmota@hotmail.com>
2024-05-14 10:19:05 -0300

    Adicionando valores globais setados no header

--- escalonador/src/App.jsx
+++ escalonador/src/App.jsx
@@ -11,12 +11,28 @@ function App() {
   // Wrapper geral do projeto
   // State para seleção de método
   const [selectedButton, setSelectedButton] = useState(0) // 0, 1, 2, 3
+  // State para Paginação
+  const [selectedPagination, setSelectedPagination] = useState(0) // 0, 1
+  // State para Quantum
+  const [quantum, setQuantum] = useState(1)
+  // State para Sobrecarga
+  const [overload, setOverload] = useState(1)
+  // State paginação
+  const [pagination, setPagination] = useState(1)
   return (
     <main className={s.wrapperMain}>
       <div className={s.internDiv}>
         <Header 
           selectedButton={selectedButton} 
           setSelectedButton={setSelectedButton}
+          selectedPagination={selectedPagination}
+          setSelectedPagination={setSelectedPagination}
+          quantum={quantum}
+          setQuantum={setQuantum}
+          overload={overload}
+          setOverload={setOverload}
+          pagination={pagination}
+          setPagination={setPagination}
         />
       </div>
     </main>
--- escalonador/src/components/Header/Header.jsx
+++ escalonador/src/components/Header/Header.jsx
@@ -2,16 +2,24 @@
 
 // Images
 import logo from '/logo.png';
+import ufba from '/ufba.png'
 // Imports
 
 // Styles
 import s from './Header.module.css';
+import { useEffect } from 'react';
 
-export default function Header({selectedButton, setSelectedButton}) {
+export default function Header({selectedButton, setSelectedButton, selectedPagination, setSelectedPagination, quantum, setQuantum, overload, setOverload, pagination, setPagination}) {
+  // useEffect(() => {
+
+  // },[quantum, setQuantum, overload, setOverload, pagination, setPagination])
   return (
-    <section className={s.wrapperHeader}>
-      <img src={logo} alt='computacao ufba'/>
-      <div className={s.displayMethod}>
+    <header className={s.wrapperHeader}>
+      <div className={s.wrapperLogos}>
+        <img src={logo} alt='computacao ufba'/>
+        <img src={ufba} alt='brasao ufba'/>
+      </div>
+      <section className={s.displayMethod}>
         <h1>Método:</h1>
         <ul className={s.methodSelection}>
           <li>
@@ -38,9 +46,34 @@ export default function Header({selectedButton, setSelectedButton}) {
               onClick={() => setSelectedButton(3)}
             >SJF</button>
           </li>
-
         </ul>
-      </div>
-    </section>
+      </section>
+      <section className={s.wrapperSettings}> 
+        <div>
+          <p>Quantum:</p>
+          <input onChange={(e) => setQuantum(e.target.value)} value={quantum} className={s.inputStyle} type='number' min='1' max='100' step='1' />
+        </div>
+        <div>
+          <p>Sobrecarga:</p>
+          <input onChange={(e) => setOverload(e.target.value)} value={overload} className={s.inputStyle} type='number' min='1' max='100' step='1' />
+        </div>
+        <div className={s.wrapperPagination}>
+          <p>Paginação:</p>
+          <button
+            className={selectedPagination == 0 ? s.selectedButton : s.notSelectedButton}
+            onClick={() => setSelectedPagination(0)}
+          >FIFO</button>
+          <button
+            className={selectedPagination == 1 ? s.selectedButton : s.notSelectedButton}
+            onClick={() => setSelectedPagination(1)}
+          >LRU</button>
+          <div className={s.wrapperPaginationSeconds}>
+            <input onChange={(e) => setPagination(parseFloat(e.target.value))} type="range" min="0.125" max="2" step={0.125} />
+            <p>{pagination}s</p>
+          </div>
+        </div>
+        
+      </section>
+    </header>
   );
 }
