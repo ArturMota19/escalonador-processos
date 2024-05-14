fbd28c7dc35f20172a678e921303ac7bca91442a
Artur <arturrodriguesmota@hotmail.com>
2024-05-14 09:29:10 -0300

    Adicionando setup do projeto

---
+++ escalonador/src/App.jsx
@@ -0,0 +1,14 @@
+import s from  './App.module.css'
+
+function App() {
+  // Wrapper geral do projeto
+  return (
+    <main className={s.wrapperMain}>
+      <div className={s.internDiv}>
+        <h1>Escalonador de Processos</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
