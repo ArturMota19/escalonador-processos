ed18489380dc27b6edde24712e5dcdd32829b5d9
bruna-anunciacao <bruunaaa04@gmail.com>
2024-05-22 14:52:34 -0300

    feat: create ganttChart and apply the sjf scheduler

--- escalonador/src/components/ShortestJob/ShortestJob.jsx
+++ escalonador/src/components/ShortestJob/ShortestJob.jsx
@@ -6,6 +6,7 @@
 import { useEffect, useState } from "react";
 // Styles
 import s from "./ShortestJob.module.css";
+import GanttChart from "../GanttChart/GanttChart";
 
 export default function ShortestJob({
   quantum,
@@ -18,34 +19,90 @@ export default function ShortestJob({
 }) {
   const [startScheduler, setStartScheduler] = useState(false);
   const [turnAroundTime, setTurnAroundTime] = useState(0);
-  const [sjfProcesses, setSjfProcesses] = useState(() => {
-    const processesCopy = [...processes];
-    processesCopy.sort((a, b) => a.time - b.time);
-    return processesCopy;
-  });
+  const [sjfProcesses, setSjfProcesses] = useState([]);
+  const [schedulerMatrix, setSchedulerMatrix] = useState([]);
+
+  useEffect(() => {
+    if (processes.length > 0) {
+      const sortedProcesses = processes
+        .filter((process) => process.status === "Waiting")
+        .sort((a, b) => {
+          if (a.arrival === b.arrival) {
+            return a.time - b.time;
+          }
+          return a.arrival - b.arrival;
+        })
+        .map((process, index) => ({
+          ...process,
+          arrivalTime: index,
+        }));
+
+      setSjfProcesses(sortedProcesses);
+    }
+  }, [processes]);
 
   const startSJF = () => {
-    setReset(false);
-    setStartScheduler(true);
-    setRamProcesses(sjfProcesses);
-    setTurnAroundTime( sjfProcesses.reduce((acc, process) => acc + process.time, 0) / sjfProcesses.length );
- };
- const resetSJF = () => {
+    if (processes.length > 0) {
+      setReset(false);
+      setStartScheduler(true);
+      setRamProcesses(sjfProcesses);
+
+      const sortedProcesses = sjfProcesses.sort((a, b) => {
+        if (a.arrival === b.arrival) {
+          return a.time - b.time;
+        }
+        return a.arrival - b.arrival;
+      });
+
+      let currentTime = 0;
+      const newSchedulerMatrix = sortedProcesses.map((process) => {
+        const startTime = Math.max(currentTime, process.arrival);
+        const endTime = startTime + process.time;
+        currentTime = endTime;
+        return { id: process.id, startTime, endTime };
+      });
+
+      setTurnAroundTime(
+        (
+          newSchedulerMatrix.reduce(
+            (acc, process) => acc + (process.endTime - process.startTime),
+            0
+          ) / newSchedulerMatrix.length
+        ).toFixed(2)
+      );
+
+      setSchedulerMatrix(newSchedulerMatrix);
+    }
+  };
+
+  const resetSJF = () => {
     setStartScheduler(false);
     setRamProcesses([]);
     setTurnAroundTime(0);
     setReset(true);
-  }
+    setSchedulerMatrix([]);
+  };
+
+  console.log(schedulerMatrix);
 
   return (
     <div className={s.sjfWrapper}>
       <div className={s.btnWrapper}>
-        <button onClick={startSJF} className={s.startBtn}>Iniciar</button>
-        <button onClick={resetSJF} className={s.resetBtn}>Reset</button>
+        <button
+          onClick={startSJF}
+          className={startScheduler ? s.disabledBtn : s.startBtn}
+          disabled={startScheduler}
+        >
+          Iniciar
+        </button>
+        <button onClick={resetSJF} className={s.resetBtn}>
+          Reset
+        </button>
       </div>
       {startScheduler && (
         <div>
           <p>TurnAround: {turnAroundTime}</p>
+          <GanttChart schedulerMatrix={schedulerMatrix} schedulerType="SJF" />
         </div>
       )}
     </div>
