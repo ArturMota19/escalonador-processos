1e6403ef7e53ce8b4540912468d8f0cc652dfab4
João Gabriel <jg.lofiego@gmail.com>
2024-07-23 17:30:35 -0300

    adiciona tempo de espera, aumenta tamanho do gantt e coloca condicao no round robin

--- escalonador/src/components/RoundRobin/RoundRobin.jsx
+++ escalonador/src/components/RoundRobin/RoundRobin.jsx
@@ -20,6 +20,9 @@ export default function RoundRobin({ quantum, overload, processes, setReset, del
       const sortedProcesses = processes
         .filter((process) => process.status === "Waiting")
         .sort((a, b) => {
+          if (a.arrival == b.arrival) {
+            return b.firstArrivalTime - a.firstArrivalTime;
+          }
           return a.arrival - b.arrival;
         })
         .map((process, index) => ({
@@ -43,6 +46,9 @@ export default function RoundRobin({ quantum, overload, processes, setReset, del
       let sortedProcesses = processes
         .filter((process) => process.status === "Waiting")
         .sort((a, b) => {
+          if (a.arrival == b.arrival) {
+            return b.firstArrivalTime - a.firstArrivalTime;
+          }
           return a.arrival - b.arrival;
         })
         .map((process, index) => ({
@@ -55,7 +61,12 @@ export default function RoundRobin({ quantum, overload, processes, setReset, del
       setrrProcesses([...sortedProcesses]);
 
       while (rrProcesses.some((process) => process.time > 0)) {
-        rrProcesses.sort((a, b) => a.arrival - b.arrival);
+        rrProcesses.sort((a, b) => {
+          if (a.arrival == b.arrival) {
+            return b.firstArrivalTime - a.firstArrivalTime;
+          }
+          return a.arrival - b.arrival;
+        });
         const process = rrProcesses.find((p) => p.time > 0 && p.arrival <= currentTime);
         if (!process) {
           currentTime++;
@@ -63,6 +74,15 @@ export default function RoundRobin({ quantum, overload, processes, setReset, del
         }
 
         const startTime = parseInt(currentTime);
+
+        process.segments.push({
+          startTime: process.arrival,
+          endTime: startTime,
+          isOverload: false,
+          isDeadlineFinished: false,
+          isWaiting: true,
+        });
+
         let remainingTime = parseInt(process.time);
         let endTime = startTime + Math.min(quantum, remainingTime);
         remainingTime -= parseInt(quantum);
@@ -92,12 +112,20 @@ export default function RoundRobin({ quantum, overload, processes, setReset, del
         process.time = parseInt(remainingTime);
 
         sortedProcesses.sort((a, b) => {
+          if (a.arrival == b.arrival) {
+            return b.firstArrivalTime - a.firstArrivalTime;
+          }
           return a.arrival - b.arrival;
         });
         setrrProcesses([...sortedProcesses]);
       }
 
-      rrProcesses.sort((a, b) => a.arrivalTime - b.arrivalTime);
+      rrProcesses.sort((a, b) => {
+        if (a.arrival == b.arrival) {
+          return b.firstArrivalTime - a.firstArrivalTime;
+        }
+        return a.arrivalTime - b.arrivalTime;
+      });
       setSchedulerMatrix(rrProcesses);
 
       setTurnAroundTime(rrProcesses.reduce((acc, value) => acc + (value.finalEndTime - value.firstArrivalTime) / rrProcesses.length, 0).toFixed(2));
